a78ef84de230cfec5a8ddf2f7cfc3020423257e8
ReenaALX <m9991333@gmail.com>
2023-07-16 16:16:51 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; X++)
+		putchar(x);
+	for (X = 'A' ; x <= 'Z' ; x++)
+		putchar(X);
+	putchar('\n');
+	return (0);
+}
