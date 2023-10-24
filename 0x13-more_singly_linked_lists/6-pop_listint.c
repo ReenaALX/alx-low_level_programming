91e7e9c6631eb6eb36c309d280655cd2311ce6d5
ReenaALX <m9991333@gmail.com>
2023-10-24 15:06:51 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,20 +1,30 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * pop_listint - deletes the head node of a listint_t linked list.
- * @head: pointer to the list.
- * Return: head node’s data (n).
- **/
+ * pop_listint - function that deletes the head node of a listint_t,
+ * linked list.
+ * @head: pointer to pointer to the head of linked list.
+ *
+ * Make sure there is no memory leaks.
+ *
+ * Return:  and returns the head node’s data (n) or 0 if list is empty.
+ */
+
 int pop_listint(listint_t **head)
 {
-	int data;
-	listint_t *first_node = *head;
+	listint_t *temp_variable; /* keep track of current head */
+	int data = 0;
 
-	if (!first_node)
+    /* if there is no list */
+	if (head == NULL || *head == NULL)
 		return (0);
-
-	data = (*head)->n;
-	*head = first_node->next;
-	free(first_node);
+	temp_variable = *head; /* stores the pointer to temp_variable head */
+    /* access the field n of temp_var and assign it to data */
+	data = temp_variable->n;
+    /* make the value at head be the value of head at next */
+	*head = (*head)->next;
+    /* free the temp var */
+	free(temp_variable);
 	return (data);
 }
