264094038b9a7b0082907726e8c45ea8eb183e2c
ReenaALX <m9991333@gmail.com>
2023-12-10 15:13:40 -0800

    2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * distint_len - returns the number of elements
+ * in a linked distint_t list.
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h);
+{
+	int counter = 0;
+
+	if (h == NULL)
+		return (counter);
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+
+	return (counter);
+}
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * distint_len - count to the number of nodes in the linked list
+ * @h: points to the beginning of a linked list
+ *
+ * Return: number of nodes
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = n;
+new_node->prev = NULL;
+new_node->next = *head;
+if (*head != NULL)
+{
+(*head)->prev = new_node;
+}
+*head = new_node;
+return (new_node);
+}
